3e914071542ce8e99f1e1993f4cd43c598a21a81
Gideon <gideonrich@hotmail.ca>
2018-03-31 15:55:08 -0600

    Add some styling and truck image

---
+++ php/template.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gideon
+ * Date: 3/31/2018
+ * Time: 1:12 PM
+ */ ?>
+
+<html lang="en-US">
+<head>
+    <title>Page Title</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../style/styles.css">
+</head>
+<body>
+
+<!-- Content wrapper -->
+<div id="content-wrapper">
+
+
+    <!-- Top of page -->
+    <div id="logo_and_title">
+        <a id="logo" title="Home" href="template.php"><img alt="Westside Auto Logo" src="http://via.placeholder.com/150x150"></a>
+        <h1 id="title">Westside Auto&trade; Database System</h1>
+    </div>
+    <!-- /Top of page -->
+
+
+    <!-- Navigation bar -->
+    <div id="nav_bar">
+        <ul>
+            <li><a class="active" href="template.php" title="Home">Home</a></li>
+            <li><a href="#forms.php" title="Forms">Forms</a></li>
+            <li><a href="#reports.php" title="Reports">Reports</a></li>
+            <li><a href="#about.php" title="About">About</a></li>
+        </ul>
+    </div>
+    <!-- /Navigation bar -->
+
+
+    <!-- Page name -->
+    <h1 id="page_name">Page name</h1>
+    <!-- /Page name -->
+
+
+    <!-- Content -->
+    <div id="page_content">
+        <p>Lorem Ipsum</p>
+    </div>
+    <!-- /Content -->
+
+    <!-- Footer -->
+    <div id="footer">
+        <small><p>CPSC 3660 - Introduction to Database Systems Group Project</p></small>
+    </div>
+    <!-- /Footer -->
+
+
+</div>
+<!-- /Content wrapper -->
+
+
+</body>
+</html>
