fc086bfa721c4996e607cc368c592fb7d2227355
Gideon <gideonrich@hotmail.ca>
2018-04-01 17:41:17 -0600

    Update form page with purchase, sale, warranty, and payment options

--- php/forms.php
+++ php/forms.php
@@ -11,6 +11,7 @@
     <title>Forms</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../style/basic_style.css">
+    <link rel="stylesheet" href="../style/forms_style.css">
 </head>
 <body>
 
@@ -45,7 +46,32 @@
 
     <!-- Content -->
     <div id="page_content">
-        <p>Lorem Ipsum</p>
+        <table id="form_table">
+            <caption>Forms to Fill Out</caption>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+            </tr>
+            <tr>
+                <td><a href="#purchase_form.php" title="Purchase Car">Purchase Car</a></td>
+                <td>Form that buyers fill out when purchasing a car. Individual problems are spotted
+                    and numbered. It is company policy to avoid cars in need of major repairs.</td>
+            </tr>
+            <tr>
+                <td><a href="#sell_form.php" title="Sell Car">Sell Car</a></td>
+                <td>A <em>salesperson</em> helps a customer select a car and negotiates the price.
+                    When the customer decides to proceed, the salesperson needs to fill out this form.</td>
+            </tr>
+            <tr>
+                <td><a href="#warranty_form.php" title="Warranty Form">Warranty</a></td>
+                <td>This form should be filled out after the <em>Sell Car Form</em>. Separate warranties are offered
+                    for various car components.</td>
+            </tr>
+            <tr>
+                <td><a href="#payment_form.php" title="Payment Form">Payment</a></td>
+                <td>Form to fill out when a customer makes a payment on their car.</td>
+            </tr>
+        </table>
     </div>
     <!-- /Content -->
 
