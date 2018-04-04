a5c206fc8cc2024140992b1f3a8d9456a887aa5b
Roddy MacCrimmon <rmaccrimm@gmail.com>
2018-04-03 22:36:45 -0600

    Added rest of foreign and primary keys

--- auto_company.sql
+++ auto_company.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 29, 2018 at 05:29 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 04, 2018 at 04:36 AM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,7 +28,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `customer`
 --
 
-CREATE TABLE `customer` (
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
   `customer_id` int(11) NOT NULL,
   `last_name` char(50) NOT NULL,
   `first_name` char(50) NOT NULL,
@@ -40,8 +41,9 @@ CREATE TABLE `customer` (
   `date_of_birth` date NOT NULL,
   `tax_id` int(11) NOT NULL,
   `avg_days_late` int(11) NOT NULL,
-  `late_payments` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `late_payments` int(11) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -49,12 +51,15 @@ CREATE TABLE `customer` (
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE IF NOT EXISTS `employee` (
   `employee_id` int(11) NOT NULL,
   `last_name` char(50) NOT NULL,
   `first_name` char(50) NOT NULL,
   `phone` char(20) NOT NULL,
-  `job_id` int(11) DEFAULT NULL
+  `job_id` int(11) NOT NULL,
+  PRIMARY KEY (`employee_id`),
+  KEY `FK_JOB_ID` (`job_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -63,9 +68,11 @@ CREATE TABLE `employee` (
 -- Table structure for table `employee_job`
 --
 
-CREATE TABLE `employee_job` (
+DROP TABLE IF EXISTS `employee_job`;
+CREATE TABLE IF NOT EXISTS `employee_job` (
+  `job_id` int(11) NOT NULL,
   `job_title` char(20) NOT NULL,
-  `job_id` int(11) NOT NULL
+  PRIMARY KEY (`job_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -74,15 +81,19 @@ CREATE TABLE `employee_job` (
 -- Table structure for table `employment_history`
 --
 
-CREATE TABLE `employment_history` (
+DROP TABLE IF EXISTS `employment_history`;
+CREATE TABLE IF NOT EXISTS `employment_history` (
   `customer_id` int(11) NOT NULL,
   `employer` char(50) NOT NULL,
   `title` char(50) NOT NULL,
   `supervisor` char(50) NOT NULL,
   `phone` char(20) NOT NULL,
   `address` char(50) NOT NULL,
-  `start` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `start` date NOT NULL,
+  `employment_hist_id` int(11) NOT NULL,
+  PRIMARY KEY (`employment_hist_id`),
+  KEY `FK_CUSTOMER_ID` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -90,13 +101,18 @@ CREATE TABLE `employment_history` (
 -- Table structure for table `payment`
 --
 
-CREATE TABLE `payment` (
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE IF NOT EXISTS `payment` (
+  `payment_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
   `payment_date` date NOT NULL,
   `due_date` date NOT NULL,
   `paid_date` date NOT NULL,
   `amount` float NOT NULL,
-  `bank_account` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `bank_account` int(11) NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  KEY `FK_CUSTOMER_ID` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -104,13 +120,17 @@ CREATE TABLE `payment` (
 -- Table structure for table `purchase`
 --
 
-CREATE TABLE `purchase` (
+DROP TABLE IF EXISTS `purchase`;
+CREATE TABLE IF NOT EXISTS `purchase` (
   `date` date NOT NULL,
   `location` char(20) NOT NULL,
   `auction` tinyint(1) NOT NULL,
-  `employee_id` int(11) NOT NULL,
-  `vehicle_id` int(11) NOT NULL,
-  `purchase_id` int(11) NOT NULL
+  `employee_id` int(11) DEFAULT NULL,
+  `vehicle_id` int(11) DEFAULT NULL,
+  `purchase_id` int(11) NOT NULL,
+  PRIMARY KEY (`purchase_id`),
+  KEY `FK_VEHICLE_ID` (`vehicle_id`),
+  KEY `FK_EMP_ID` (`employee_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -119,15 +139,34 @@ CREATE TABLE `purchase` (
 -- Table structure for table `sale`
 --
 
-CREATE TABLE `sale` (
+DROP TABLE IF EXISTS `sale`;
+CREATE TABLE IF NOT EXISTS `sale` (
   `sale_id` int(11) NOT NULL,
-  `employee_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
+  `employee_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
   `date` date NOT NULL,
   `total_due` decimal(10,0) NOT NULL,
   `down_payment` decimal(10,0) NOT NULL,
-  `finance_amount` decimal(10,0) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `finance_amount` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`sale_id`),
+  KEY `FK_CUSTOMER_ID` (`customer_id`),
+  KEY `FK_EMP_ID` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_comission`
+--
+
+DROP TABLE IF EXISTS `sales_comission`;
+CREATE TABLE IF NOT EXISTS `sales_comission` (
+  `employee_id` int(11) DEFAULT NULL,
+  `comission_amount` float NOT NULL,
+  `commission_id` int(11) NOT NULL,
+  PRIMARY KEY (`commission_id`),
+  KEY `FK_EMP_ID` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -135,7 +174,8 @@ CREATE TABLE `sale` (
 -- Table structure for table `vehicle`
 --
 
-CREATE TABLE `vehicle` (
+DROP TABLE IF EXISTS `vehicle`;
+CREATE TABLE IF NOT EXISTS `vehicle` (
   `vehicle_id` int(11) NOT NULL,
   `miles` int(11) NOT NULL,
   `vehicle_condition` int(11) NOT NULL,
@@ -146,8 +186,9 @@ CREATE TABLE `vehicle` (
   `interior_color` char(10) NOT NULL,
   `make` char(20) NOT NULL,
   `model` char(20) NOT NULL,
-  `year` year(4) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `year` year(4) NOT NULL,
+  PRIMARY KEY (`vehicle_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -155,9 +196,11 @@ CREATE TABLE `vehicle` (
 -- Table structure for table `vehicle_owner`
 --
 
-CREATE TABLE `vehicle_owner` (
+DROP TABLE IF EXISTS `vehicle_owner`;
+CREATE TABLE IF NOT EXISTS `vehicle_owner` (
   `customer_id` int(11) NOT NULL,
-  `vehicle_id` int(11) NOT NULL
+  `vehicle_id` int(11) NOT NULL,
+  PRIMARY KEY (`customer_id`,`vehicle_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -166,12 +209,16 @@ CREATE TABLE `vehicle_owner` (
 -- Table structure for table `vehicle_problem`
 --
 
-CREATE TABLE `vehicle_problem` (
+DROP TABLE IF EXISTS `vehicle_problem`;
+CREATE TABLE IF NOT EXISTS `vehicle_problem` (
+  `problem_id` int(11) NOT NULL,
   `description` char(50) NOT NULL,
   `purchase_id` int(11) NOT NULL,
   `estimated_cost` int(11) NOT NULL,
-  `actual_cost` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `actual_cost` int(11) NOT NULL,
+  PRIMARY KEY (`problem_id`) USING BTREE,
+  KEY `FK_PURCHASE_ID` (`purchase_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -179,15 +226,20 @@ CREATE TABLE `vehicle_problem` (
 -- Table structure for table `warranty`
 --
 
-CREATE TABLE `warranty` (
-  `vehicle_id` int(11) NOT NULL,
-  `employee_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `warranty`;
+CREATE TABLE IF NOT EXISTS `warranty` (
+  `vehicle_id` int(11) DEFAULT NULL,
+  `employee_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
   `warranty_id` int(11) NOT NULL,
   `sale_date` date NOT NULL,
   `total_cost` decimal(10,0) NOT NULL,
-  `monthly_cost` decimal(10,0) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `monthly_cost` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`warranty_id`),
+  KEY `FK_VEHICLE_ID` (`vehicle_id`),
+  KEY `FK_EMP_ID` (`employee_id`),
+  KEY `FK_CUSTOMER_ID` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -195,68 +247,18 @@ CREATE TABLE `warranty` (
 -- Table structure for table `warranty_item`
 --
 
-CREATE TABLE `warranty_item` (
+DROP TABLE IF EXISTS `warranty_item`;
+CREATE TABLE IF NOT EXISTS `warranty_item` (
+  `item_id` int(11) NOT NULL,
   `warranty_id` int(11) NOT NULL,
   `item_covered` char(20) NOT NULL,
   `start_date` date NOT NULL,
   `length` int(11) NOT NULL,
   `cost` decimal(10,0) NOT NULL,
-  `deductible` decimal(10,0) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customer_id`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`employee_id`),
-  ADD KEY `FK_JOB_ID` (`job_id`) USING BTREE;
-
---
--- Indexes for table `employee_job`
---
-ALTER TABLE `employee_job`
-  ADD PRIMARY KEY (`job_id`);
-
---
--- Indexes for table `purchase`
---
-ALTER TABLE `purchase`
-  ADD PRIMARY KEY (`purchase_id`),
-  ADD KEY `FK_EMP_ID` (`employee_id`);
-
---
--- Indexes for table `sale`
---
-ALTER TABLE `sale`
-  ADD PRIMARY KEY (`sale_id`);
-
---
--- Indexes for table `vehicle`
---
-ALTER TABLE `vehicle`
-  ADD PRIMARY KEY (`vehicle_id`);
-
---
--- Indexes for table `vehicle_problem`
---
-ALTER TABLE `vehicle_problem`
-  ADD PRIMARY KEY (`purchase_id`);
-
---
--- Indexes for table `warranty`
---
-ALTER TABLE `warranty`
-  ADD PRIMARY KEY (`warranty_id`);
+  `deductible` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `FK_WARRANTY_ID` (`warranty_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
@@ -266,13 +268,59 @@ ALTER TABLE `warranty`
 -- Constraints for table `employee`
 --
 ALTER TABLE `employee`
-  ADD CONSTRAINT `job_id` FOREIGN KEY (`job_id`) REFERENCES `employee_job` (`job_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `job_id_constraint` FOREIGN KEY (`job_id`) REFERENCES `employee_job` (`job_id`);
+
+--
+-- Constraints for table `employment_history`
+--
+ALTER TABLE `employment_history`
+  ADD CONSTRAINT `employment_hist_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `purchase`
 --
 ALTER TABLE `purchase`
-  ADD CONSTRAINT `employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `purchase_emp_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `purchase_vehicle_id` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`vehicle_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sale`
+--
+ALTER TABLE `sale`
+  ADD CONSTRAINT `sale_ customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `sale_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sales_comission`
+--
+ALTER TABLE `sales_comission`
+  ADD CONSTRAINT `commission_emp_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `vehicle_problem`
+--
+ALTER TABLE `vehicle_problem`
+  ADD CONSTRAINT `problem_purchase_id` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`purchase_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `warranty`
+--
+ALTER TABLE `warranty`
+  ADD CONSTRAINT `warranty_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `warranty_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`employee_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `warranty_vehicle_id` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`vehicle_id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `warranty_item`
+--
+ALTER TABLE `warranty_item`
+  ADD CONSTRAINT `item_warranty_id` FOREIGN KEY (`warranty_id`) REFERENCES `warranty` (`warranty_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
