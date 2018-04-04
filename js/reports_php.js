b4bf2d77505ab741c0d14d341d7564a1d1c1ddf6
Gideon <gideonrich@hotmail.ca>
2018-04-04 00:23:29 -0600

    Add some comments

--- js/reports_php.js
+++ js/reports_php.js
@@ -1,18 +1,26 @@
 $(function() {
+    // When report form is 'Generated' run this:
     $("#report_form").on('submit', function(e) {
+
+        // Prepare values of form
         var values = $(this).serialize();
 
+        // Prepare ajax and send request to php
         $.ajax({
             url: "../php/report_response.php",
             type: "post",
             data: values,
             success: function (response) {
+                // Replace div content with response
                 $("#form_ajax_result").html(response);
             },
+                // If there's an error, log it TODO: Doesn't work
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log(textStatus, errorThrown);
             }
         });
+        // Normal form event is to go to form:action, which
+        // by default refreshes the same page. Don't do that.
         e.preventDefault();
     });
 });
