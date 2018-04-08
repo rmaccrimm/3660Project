1428f49056c3b343dc8a9d01ffafec53681259e1
Roddy MacCrimmon <rmaccrimm@gmail.com>
2018-04-08 15:48:08 -0600

    Added employee report sql

--- php/reports/customer_report_response.php
+++ php/reports/customer_report_response.php
@@ -9,37 +9,37 @@ include '../database_functions.php';
 include 'report_functions.php';
 
 $full_name = $_POST['name_hint'];
-
 echo '<p>'.$full_name.'</p>';
 
 $names = explode(' ', $full_name);
 $first = strtoupper($names[0]);
 $last = strtoupper($names[1]);
+
 $contact_query =
-            'SELECT first_name, last_name, address, city, state, zip_code, phone
-             FROM customer
-             WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\'';
+    'SELECT first_name, last_name, address, city, state, zip_code, phone
+     FROM customer
+     WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\'';
 $vehicle_query =
-            'SELECT make, model, year, exterior_color, interior_color, style, vehicle_condition, miles
-             FROM customer, vehicle, vehicle_owner
-             WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\' 
-                   AND customer.customer_id=vehicle_owner.customer_id 
-                   AND vehicle.vehicle_id=vehicle_owner.vehicle_id';
+    'SELECT make, model, year, exterior_color, interior_color, style, vehicle_condition, miles
+     FROM customer, vehicle, vehicle_owner
+     WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\' 
+           AND customer.customer_id=vehicle_owner.customer_id 
+           AND vehicle.vehicle_id=vehicle_owner.vehicle_id';
 $payment_query =
-            'SELECT payment_info.tax_id, payment_info.avg_days_late, payment_info.late_payments
-             FROM customer, payment_info
-             WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\' 
-                   AND customer.customer_id=payment_info.customer_id';
+    'SELECT payment_info.tax_id, payment_info.avg_days_late, payment_info.late_payments
+     FROM customer, payment_info
+     WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\' 
+           AND customer.customer_id=payment_info.customer_id';
 $warranty_cost_query =
-            'SELECT employee.first_name, employee.last_name, sale_date, total_cost, monthly_cost
-             FROM customer, warranty, employee
-             WHERE customer.first_name=\''.$first.'\' AND customer.last_name=\''.$last.'\'
-                   AND warranty.customer_id=customer.customer_id AND warranty.employee_id=employee.employee_id';
+    'SELECT employee.first_name, employee.last_name, sale_date, total_cost, monthly_cost
+     FROM customer, warranty, employee
+     WHERE customer.first_name=\''.$first.'\' AND customer.last_name=\''.$last.'\'
+           AND warranty.customer_id=customer.customer_id AND warranty.employee_id=employee.employee_id';
 $warranty_item_query =
-            'SELECT item_covered, start_date, length, cost, deductible
-             FROM warranty_item, customer, warranty
-             WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\'
-                   AND warranty.customer_id=customer.customer_id AND warranty_item.warranty_id=warranty.warranty_id';
+    'SELECT item_covered, start_date, length, cost, deductible
+     FROM warranty_item, customer, warranty
+     WHERE first_name=\''.$first.'\' AND last_name=\''.$last.'\'
+           AND warranty.customer_id=customer.customer_id AND warranty_item.warranty_id=warranty.warranty_id';
 
 make_table(
     db_query($contact_query),
