a22047449b5cec95d7979888579fa2c8a58e7298
Gideon <gideonrich@hotmail.ca>
2018-04-08 17:18:49 -0600

    adding delete button

--- js/purchase_form.js
+++ js/purchase_form.js
@@ -18,24 +18,46 @@ $(function() {
     new_problem_button.on('click', function(e) {
         e.preventDefault();
 
+        let div_id = format_str_and_counter('problem', problem_counter);
         let description_id = format_str_and_counter('problem_desc', problem_counter);
         let estimated_cost_id = format_str_and_counter('problem_estimated_cost', problem_counter);
         let actual_cost_id = format_str_and_counter('problem_actual_cost', problem_counter);
+        let delete_button_id = format_str_and_counter('delete_problem', problem_counter);
 
         let new_elements = "<br><hr>";
+        new_elements = "<div id=" + div_id + ">";
+
+        // Problem counter is incremented by one to avoid 'Problem 0' being displayed
         new_elements += "<p>Problem " + (problem_counter + 1) + "</p>";
+
+        // Delete problem button
+        new_elements += "<button class='delete_button'>Delete</button>";
+
+        // Problem Description Text Box
         new_elements += "<label for='problem_desc'>Description</label>";
+
         new_elements += "<input type='text' name=" + description_id + " id=" + description_id + ">";
-
+        // Estimated Cost Input
         new_elements += "<label for='problem_estimated_cost'>$ Estimated Cost</label>";
-        new_elements += "<input value='0' type='number' min='0' step='10' name=" + estimated_cost_id + " id=" + estimated_cost_id + ">";
 
+        new_elements += "<input value='0' type='number' min='0' step='10' name=" + estimated_cost_id + " id=" + estimated_cost_id + ">";
+        //Actual Cost Input
         new_elements += "<label for='problem_actual_cost'>$ Actual Cost</label>";
+
         new_elements += "<input value='0' type='number' min='0' step='10' name=" + actual_cost_id + " id=" + actual_cost_id + ">";
 
+        new_elements += "</div>";
+
+        // We want the new problem button to be at the bottom all the time
         $(new_elements).insertBefore('#add_new_problem_button');
 
         problem_counter += 1;
+    });
+
+    var delete_problem_button = $('.delete_button');
+
+    delete_problem_button.on('click', function(e){
+
     });
 
     function format_str_and_counter(str, counter) {
